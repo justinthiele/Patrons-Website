4bdbbd4d7ba0b431983d9d2eba6ef420ac0c8692
Justin Thiele <justin.thiele@gmail.com>
2012-05-06 20:38:59 -0700

    open source commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Patrons Website
+
+Patrons is a Facebook app to accept monthly donations from your biggest fans. This is the Patrons website at [http://patrons.us](http://patrons.us).
---
+++ config/routes.rb
@@ -0,0 +1,72 @@
+SupportersclubFront::Application.routes.draw do
+  
+root :to => 'high_voltage/pages#show', :id => 'home'
+  
+
+match '/home' => 'high_voltage/pages#show', :id => 'home'
+match '/learn-more' => 'high_voltage/pages#show', :id => 'learn-more'
+match '/contact' => 'high_voltage/pages#show', :id => 'contact'
+match '/about' => 'high_voltage/pages#show', :id => 'about'
+match '/faq' => 'high_voltage/pages#show', :id => 'faq'
+match '/get-started' => 'high_voltage/pages#show', :id => 'get-started'
+match '/terms' => 'high_voltage/pages#show', :id => 'terms'
+
+
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
+end
